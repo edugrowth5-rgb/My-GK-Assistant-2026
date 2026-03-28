23e89fc73535ced7e97e748368b67894b3a8da40
edugrowth5-rgb <edugrowth5@gmail.com>
2026-03-28 19:53:22 +0530

    Create GKsw.js

---
+++ GKsw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'gk-pro-v1';
+const ASSETS = [
+  'GKindex.html',
+  'GKstyle.css',
+  'GKapp.js',
+  'GKmanifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch Assets from Cache if Offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
